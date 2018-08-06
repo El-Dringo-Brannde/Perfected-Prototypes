8a0a75835de07290a89c5e6bc0d8b589dacdda37
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2018-08-06 13:04:16 -0700

    Added tests for safe access

--- src/Object/index.js
+++ src/Object/index.js
@@ -1,50 +1,11 @@
 const merger = require('./../merger')
-const chalk = require('chalk')
-const log = function(log) { return console.log(chalk.red(log)) }
+const safeD = require('lodash/get')
 
 const newObject = {};
 
-newObject.safeD = function(dereference) {
-   const objectDerefs = dereference.split('.')
-
-   let lastDeref = this
-   let badDereference = objectDerefs.some(deref => {
-      console.log(deref)
-      const arrayCheck = deref.match(/\[(.*?)\]/g).map(b => b.replace(/\[(|(.*?)\)/g, "$1"))
-
-      // if there is array dereferencing and it's false
-      if (arrayCheck && !lastDeref[arrayCheck[1]])
-         return true
-
-      if (!lastDeref[deref])
-         return true
-      else {
-         lastDeref = lastDeref[deref]
-         return false
-      }
-   });
-
-   if (badDereference && objectDerefs.length > 1)
-      return false
-   else
-      return lastDeref
+newObject.get = function(dereference) {
+   return safeD(this, dereference, false)
 }
 
 
 merger(Object.prototype, newObject)
-
-
-// console.log(newObject.safeD('foo'))
-
-
-// Object.defineProperty(Object.prototype, 'safeD', {
-//    value: function(dereference) {
-//       console.log(this)
-//       return 1
-//    },
-//    writable: true
-// });
-
-// const f = { a: 2 }
-
-// console.log(f.safeD('foo'))
--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 require('./Object/index.js')
-
-
-let f = { a: { b: 1 } }
-console.log(f.safeD('a'))
-console.log(f.safeD('a["b"]'))
-
-console.log(f.safeD('a.b'))
-console.log(f.safeD('a.b.c'))
-console.log(f.safeD('a.b.c[0]'))
-console.log(f.safeD('a["b"]["c"][0]'))
