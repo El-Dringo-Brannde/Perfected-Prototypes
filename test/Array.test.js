d1e0d4b6380b6641c150491e1dd0f4bf1ee5a174
Devin Roche <Roche.devin@ymail.com>
2018-08-06 10:25:25 -0700

    add shuffle function and test

--- test/Array.test.js
+++ test/Array.test.js
@@ -2,13 +2,17 @@ import {expect} from 'chai';
 import '../src/Array/index';
 
 describe('array prototype', () => {
-    const arr = [1, 2, 3];
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     it('should return 1' , () => {
         expect(arr.first()).to.equal(1);
     })
 
     it('should return 3' , () => {
-        expect(arr.last()).to.equal(3);
+        expect(arr.last()).to.equal(9);
+    })
+
+    it('should return 3' , () => {
+        expect(arr.shuffle()).to.not.deep.equal(arr);
     })
 })
