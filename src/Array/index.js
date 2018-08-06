a0f4b1538667cff5db4e67efa4e64e8d4e498fe5
Devin Roche <Roche.devin@ymail.com>
2018-08-06 11:10:26 -0700

    add types function and range array

--- src/Array/index.js
+++ src/Array/index.js
@@ -1,6 +1,5 @@
 Array.prototype.first = function() {
     try {
-        console.log(this)
         return this[0]
     } catch(err) {
         return err
@@ -9,11 +8,26 @@ Array.prototype.first = function() {
 
 Array.prototype.last = function() {
     try {
-        console.log(this)
         return this[this.length-1]
     } catch(err) {
         return err
     }
+}
+
+Array.prototype.types = function(){
+    try {
+        return this.map(el => typeof el)
+    } catch(err) {
+        throw new Error('unexpected error encountered')
+    }
+}
+
+Array.prototype.rangeArr = function(low, high, size) {
+    while(size > 0) {
+        this.push(Math.floor(Math.random() * high) + low)
+        size--
+    }
+    return this
 }
 
 export default Array;
--- test/Array.test.js
+++ test/Array.test.js
@@ -3,6 +3,8 @@ import '../src/Array/index';
 
 describe('array prototype', () => {
     const arr = [1, 2, 3];
+    const arrType = [1, 'hi', {a: 1}]
+    const rangeArr = []
 
     it('should return 1' , () => {
         expect(arr.first()).to.equal(1);
@@ -10,5 +12,13 @@ describe('array prototype', () => {
 
     it('should return 3' , () => {
         expect(arr.last()).to.equal(3);
+    })
+
+    it('should return 3' , () => {
+        expect(arrType.types()).to.deep.equal(['number', 'string', 'object']);
+    })
+
+    it('should return random array in range' , () => {
+        expect(rangeArr.rangeArr(3, 30, 5)).to.have.length(5);
     })
 })
